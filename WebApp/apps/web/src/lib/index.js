8387c72c1e59d6d50abdbdf67cb9858ca8a560bd
Sipho-Haldan <siphontobela@haldanconsulting.com>
2026-06-22 10:23:43 +0200

    Index + app.css

--- WebApp/apps/web/src/lib/index.js
+++ WebApp/apps/web/src/lib/index.js
@@ -1,1 +1,5 @@
-// place files you want to import through the `$lib` alias in this folder.
+export { default as Badge } from './components/atoms/Badge.svelte';
+export { default as Button } from './components/atoms/Button.svelte';
+export { default as MetricCard } from './components/molecules/MetricCard.svelte';
+export { default as NavItem } from './components/molecules/NavItem.svelte';
+export { default as SearchInput } from './components/molecules/SearchInput.svelte';
